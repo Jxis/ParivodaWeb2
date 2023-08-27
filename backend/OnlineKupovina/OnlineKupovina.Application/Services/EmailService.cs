d588e85a9a432465060a26fca1dfda896f92b67d
Natasa Gavrilovic <gavrilovicnatasa123@gmail.com>
2023-08-27 15:06:45 +0200

    User done

---
+++ backend/OnlineKupovina/OnlineKupovina.Application/Services/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using System;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using OnlineKupovina.Application.ServiceInterfaces;
+using System.Text.RegularExpressions;
+
+namespace OnlineKupovina.Application.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+
+            _config = config;
+        }
+
+        public async Task SendEmail(string to, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
+            message.To.Add(MailboxAddress.Parse(to));
+            message.Subject = subject;
+
+            message.Body = new TextPart(TextFormat.Plain)
+            {
+                Text = body
+            };
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+            await smtp.SendAsync(message);
+            await smtp.DisconnectAsync(true);
+        }
+
+        public bool IsValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+            Regex regex = new(pattern);
+
+            return regex.IsMatch(email);
+        }
+
+    }
+}
